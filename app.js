85b08aa7233ffc19a8bc57c91c3de80626bfa81a
SamuelNakab <167198088+SamuelNakab@users.noreply.github.com>
2025-09-02 12:06:36 -0300

    2/9

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import {config} from './dbconfig.js'
 import express from "express";
 import 'dotenv/config'
-
+import bcrypt from 'bcrypt'
 import pkg from 'pg'
 const {Client} = pkg;
 
@@ -26,6 +26,54 @@ app.get('/canciones', async (req, res) => {
   console.log(result.rows);
   res.send(result.rows)
 
+})
+
+app.post('/createuser', async (req,res) =>{
+    const user = req.body;
+    if(!user.nombre || !user.userID || !user.password){
+        return res.status(400).json({mesagge : "Debe completar todos los campos"});
+    }
+    try {
+        const client = new Client(config);
+        await client.connect();
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        let result = await client.query('insert into usuario values($1, $2, $3) returning',[
+            user.userid, user.nombre, user.password
+        ]);
+        await client.end();
+        console.log('Rows creadas: ' + result.rowCount);
+
+        console.log(result.rows);
+        res.send(result.rows)
+    } catch (error) {
+        return res.status(500).json({mesagge : error.mesagge});
+    }
+});
+
+app.post('/login', async (req,res) =>{
+    const user = req.body;
+    if (!user.userid || user.password) {
+        return res.status(400).json({mesagge : 'Debe completar todos los campos'});
+    }
+    try {
+        let result = await client.query('select * from usuario where user_id  = $1',[user.userid]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({mesagge : 'Usuario no encontrado'});
+        }
+
+        let dbUser = result.rows[0];
+        const passOK = await bcrypt.compare(user.password, dbUser.password);
+        //
+        //  crear token y con payload
+
+        if (passOK) {
+            res.send({nombre : dbUser.nombre, token});
+        }else{
+            res.send("Clave invalida")
+        }
+    } catch (error) {
+        return res.status(500).json({mesagge : error.mesagge});
+    }
 })
 
 app.listen(PORT, () => {
